a94080706f5cebcc5a5601919863fc7713b86fe4
gbatubara <gabrielbatubara91@gmail.com>
2021-09-10 12:06:42 +0700

    update javascript

--- assets/script.js
+++ assets/script.js
@@ -13,12 +13,8 @@ const updateScreen = (number) => {
 }
 
 let prevNumber = ''
-let calculatorOperator = ''
+let calculationOperator = ''
 let currentNumber = '0'
-
-const inputNumber = (number) => {
-    currentNumber = number
-}
 
 numbers.forEach((number) => {
     number.addEventListener("click", (event) => {
@@ -28,13 +24,81 @@ numbers.forEach((number) => {
 })
 
 const inputNumber = (number) => {
-    currentNumber += number
-}
-
-const inputNumber = (number) => {
     if (currentNumber === '0') {
         currentNumber = number
     } else {
         currentNumber += number
     }
 }
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    if (calculationOperator === '') {
+        prevNumber = currentNumber
+    }
+    calculationOperator = operator
+    currentNumber = '0'
+}
+
+const equals = document.querySelector('.equal')
+
+equals.addEventListener('click', () => {
+    calculate()
+    updateScreen(currentNumber)
+})
+
+const calculate = () => {
+    let result = ''
+    switch(calculationOperator) {
+        case '+':
+            result = parseFloat(prevNumber) + parseFloat(currentNumber)
+            break
+        case '-':
+            result = parseFloat(prevNumber) - parseFloat(currentNumber)
+            break
+        case '*':
+            result = parseFloat(prevNumber) * parseFloat(currentNumber)
+            break
+        case '/':
+            result = parseFloat(prevNumber) / parseFloat(currentNumber)
+            break
+        default:
+            return
+    }
+    currentNumber = result
+    calculationOperator = ''
+}
+
+const clearBtn = document.querySelector('.all-clear')
+
+clearBtn.addEventListener('click', () => {
+    clearAll()
+    updateScreen(currentNumber)
+})
+
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
+}
+
+const decimal = document.querySelector('.decimal')
+
+const inputDecimal = (dot) => {
+    if (currentNumber.includes('.')) {
+        return
+    }
+    currentNumber += dot
+}
+
+decimal.addEventListener('click', (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
